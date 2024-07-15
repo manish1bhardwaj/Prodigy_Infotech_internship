7c9062ec4131fc44c4ed6ae05ccc236ebe2c4953
manish1bhardwaj <talktomanishbhardwaj0010@gmail.com>
2024-07-15 10:18:01 +0530

    first commit

---
+++ Task_01/script.js
@@ -0,0 +1,10 @@
+// script.js
+
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
